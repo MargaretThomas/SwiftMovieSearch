c80cd640ebc8ff37d5208e0d66ec439b7d568532
Margaret Thomas <magz.thomas@gmail.com>
2016-12-23 11:05:30 +0200

    Base Project

---
+++ MusicSearch/MusicSearch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MusicSearch
+//
+//  Created by HS User 3 on 2016/12/23.
+//  Copyright © 2016 immedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
